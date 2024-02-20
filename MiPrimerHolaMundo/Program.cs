49df9896ef93b90a8e3e013a23120437a479b03b
alejandro atehortua <ateho1705@hotmail.com>
2024-02-20 15:29:32 -0500

    Codigo de mi primer hola mundo

---
+++ MiPrimerHolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerHolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo, como estan? Espero esten muy bien!!");
+        }
+    }
+}
