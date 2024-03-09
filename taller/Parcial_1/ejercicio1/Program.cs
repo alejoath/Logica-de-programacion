7b559b05f55cd2a51963176630be35b447d8d8c7
alejandro atehortua <ateho1705@hotmail.com>
2024-03-08 20:36:54 -0500

    se responde la segunda pregunta

--- taller/Parcial_1/ejercicio1/Program.cs
+++ taller/Parcial_1/ejercicio1/Program.cs
@@ -12,13 +12,13 @@ namespace ejercicio1
             //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
 
             // declaracion de constantes
-            const double sueldoBase = 1380000;
+            const double sueldobase = 1380000;
             const double porcentajeComision = 0.10;
             const double objetivoVentas = 1000000;
 
             // declaracion de variables
 
-            double venta1, venta2, venta3, comisiontotal;
+            double venta1, venta2, venta3, comisiontotal, salariototal;
 
             Console.WriteLine("Ingrese el valor de la primera venta: ");
             venta1 = double.Parse(Console.ReadLine());
@@ -32,7 +32,13 @@ namespace ejercicio1
             //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes?
 
             comisiontotal = (venta1 + venta2 + venta3) * porcentajeComision;
-            Console.WriteLine($"Comisión total: ${comisiontotal:N0}");
+            Console.WriteLine($"Comisión total: {comisiontotal:N0}");
+
+            //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
+
+            salariototal = sueldobase + comisiontotal;
+            Console.WriteLine($"Salario total: {salariototal:N0}");
+
 
         }
     }
