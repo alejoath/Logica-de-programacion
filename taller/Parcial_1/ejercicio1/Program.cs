6f3778c4011f61374265ba56dd956835d1de5e55
alejandro atehortua <ateho1705@hotmail.com>
2024-03-08 21:20:29 -0500

    se limpia espacio

--- taller/Parcial_1/ejercicio1/Program.cs
+++ taller/Parcial_1/ejercicio1/Program.cs
@@ -7,12 +7,11 @@ namespace ejercicio1
     {
         static void Main(string[] args)
         { 
-            // declaracion de constantes
             const double sueldobase = 1380000;
             const double porcentajeComision = 0.10;
             const double objetivoventas = 1000000;
 
-            // declaracion de variables
+            //variables
 
             double venta1, venta2, venta3, comisiontotal, salariototal, promediocomision;
 
@@ -25,17 +24,11 @@ namespace ejercicio1
             Console.WriteLine("Ingrese el valor de la tercera venta: ");
             venta3 = double.Parse(Console.ReadLine());
 
-            //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes?
-
             comisiontotal = (venta1 + venta2 + venta3) * porcentajeComision;
             Console.WriteLine($"Comisión total: ${comisiontotal:N0}");
 
-            //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
-
             salariototal = sueldobase + comisiontotal;
             Console.WriteLine($"Salario total: ${salariototal:N0}");
-
-            //¿Cuál fue la venta que le generó la mayor comisión?
 
             double mayor = venta1;
 
@@ -47,14 +40,8 @@ namespace ejercicio1
 
             Console.WriteLine($"La venta que mas genero comision es: {mayor :N0}");
 
-            //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
-
             promediocomision = comisiontotal / 3;
             Console.WriteLine($"Promedio de comisiones por ventas: ${promediocomision:N0}");
-
-            //Además, la empresa da un beneficio extra de $100.000 si el vendedor supera el objetivo del mes.
-            //El objetivo es sumar en las 3 ventas como mínimo $1.000.000 en ventas.
-            //Mostrar un mensaje donde indique si ganó o no el beneficio.
 
             if (venta1 + venta2 + venta3 >= objetivoventas) Console.WriteLine("¡GANO BENEFICIO EXTRA POR VENTAS!");
             else Console.WriteLine("No se cumplio con el objetivo de ventas :(");
