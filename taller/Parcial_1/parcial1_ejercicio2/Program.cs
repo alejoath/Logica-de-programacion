5094567357b5f73595c91586812eb2e40b33b1ba
alejandro atehortua <ateho1705@hotmail.com>
2024-03-08 21:35:11 -0500

    declaracion de variables

---
+++ taller/Parcial_1/parcial1_ejercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace parcial1_ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de constantes
+            const double velocidadTren1 = 80, velocidadTren2 = 100; 
+
+
+        }
+    }
+}
