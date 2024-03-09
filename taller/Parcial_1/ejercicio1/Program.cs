b071efc52a4fd5b0f90664fd13a55ad4f1b58a00
alejandro atehortua <ateho1705@hotmail.com>
2024-03-08 20:19:26 -0500

    declaracion de constantes

---
+++ taller/Parcial_1/ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un
+            //sueldo base de $XXXX.Además, recibe una comisión del 10 % sobre el valor total de cada venta que
+            //realiza.El vendedor desea saber:
+            //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el
+            //mes?
+            //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
+            //¿Cuál fue la venta que le generó la mayor comisión?
+            //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
+
+            // declaracion de constantes
+            const double sueldoBase = 1380000;
+            const double porcentajeComision = 0.10;
+            const double objetivoVentas = 1000000;
+        }
+    }
+}
