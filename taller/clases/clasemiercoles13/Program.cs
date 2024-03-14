6bebcf060115d05b403631dea7e098753b1c1aab
alejandro atehortua <ateho1705@hotmail.com>
2024-03-13 21:27:10 -0500

    codigo de la clase ejercicio 13

--- taller/clases/clasemiercoles13/Program.cs
+++ taller/clases/clasemiercoles13/Program.cs
@@ -6,7 +6,30 @@ namespace clasemiercoles13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Construir un algoritmo que pida N números por pantalla,
+            //cuando el usuario ingrese un número negativo,
+            //el programa debe finalizar y mostrar en consola la cantidad total de números positivos que ingresó
+            //hasta antes de ingresarse el número negativo.
+
+            int num, contador = 0;
+            bool validador = true;
+
+            //creacion del ciclo
+
+            while (validador)
+            {
+
+                Console.WriteLine("ingrese un numero: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num < 0) validador = false;
+                else contador = contador + 1;
+
+            }
+            Console.WriteLine(String.Format("El total de numero positivos ingresados es de: {0}", contador));
+
+
+
         }
     }
 }
