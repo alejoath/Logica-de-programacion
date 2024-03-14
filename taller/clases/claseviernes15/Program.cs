4c8e8a6e44cc9ab1396c4aefa4c4bb58557f40ea
alejandro atehortua <ateho1705@hotmail.com>
2024-03-13 20:48:43 -0500

    clase viernes

---
+++ taller/clases/claseviernes15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace claseviernes15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hola")
+        }
+    }
+}
