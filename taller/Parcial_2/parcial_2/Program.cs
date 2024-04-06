5a211e7a1101339ab5d6d93f86a4af12aec1e63d
alejandro atehortua <ateho1705@hotmail.com>
2024-04-05 20:25:54 -0500

    Bienvenida al juego y establecimiento de numero de jugadores

--- taller/Parcial_2/parcial_2/Program.cs
+++ taller/Parcial_2/parcial_2/Program.cs
@@ -6,7 +6,36 @@ namespace parcial_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaracion de variables
+
+            int numerodejugadores, maximo = 0, minimo = 0;
+
+            Console.WriteLine("BIENVENIDIOS AL JUEGO: ¡Adivina el numero!");
+
+            Console.Write("Ingrese la cantidad de jugadores entre (minimo 2 y maximo 4): ");
+            numerodejugadores = int.Parse(Console.ReadLine());
+            
+            switch (numerodejugadores)
+            {
+                case 2:
+                    maximo = 50;
+                    break;
+                case 3:
+                    maximo = 100;
+                    break;
+                case 4:
+                    maximo = 200;
+                    break;
+                default:
+                    Console.WriteLine("Cantidad de jugadores no válida. El juego se cerrará.");
+                    return;
+            }
+
+
+
+
+
+
         }
     }
 }
